e60cc96bc9d7673f0b92a17b51ec823b2552009f
Andy <klee682@aucklanduni.ac.nz>
2024-07-24 23:37:15 +1200

    update

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./src/routes/userRoutes');
-const dashboardRoutes = require('./src/routes/dashboardRoutes'); // Import dashboardRoutes
-const chatRoutes = require('./src/routes/chatRoutes'); // Import chatRoutes
-const groupChatRoutes = require('./src/routes/groupChatRoutes'); // Import groupChatRoutes
-require('dotenv').config(); // Load environment variables from .env file
+const dashboardRoutes = require('./src/routes/dashboardRoutes');
+const chatRoutes = require('./src/routes/chatRoutes');
+const groupChatRoutes = require('./src/routes/groupChatRoutes');
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5000; // Fallback to port 5000 if PORT is not set
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://andy:helloyellow123@messageapp.wjpp3oh.mongodb.net/';
 
 // Middleware
 app.use(bodyParser.json());
@@ -19,15 +19,13 @@ app.use(cors()); // Enable CORS for all origins
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/chat', chatRoutes); 
+app.use('/api/chat', chatRoutes);
 app.use('/api/groupchat', groupChatRoutes);
 
 // Database connection
-const mongoURI = 'mongodb://localhost:27017/ChatStream';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect(mongoURI)
+
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${port}`);
 });
