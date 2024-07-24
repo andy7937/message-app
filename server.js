6be274b528be834e6e8ad8af07188282183b4b34
Andy Lee <andy@wifi-172-23-34-174.uoa-wifi.auckland.ac.nz>
2024-07-24 16:52:46 +1200

    deploying heroku

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./src/routes/userRoutes');
+const dashboardRoutes = require('./src/routes/dashboardRoutes'); // Import dashboardRoutes
+const chatRoutes = require('./src/routes/chatRoutes'); // Import chatRoutes
+const groupChatRoutes = require('./src/routes/groupChatRoutes'); // Import groupChatRoutes
+require('dotenv').config(); // Load environment variables from .env file
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all origins
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/dashboard', dashboardRoutes);
+app.use('/api/chat', chatRoutes); 
+app.use('/api/groupchat', groupChatRoutes);
+
+// Database connection
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/ChatStream';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+// src/routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/:username', userController.getUser);
+
+
+module.exports = router;
