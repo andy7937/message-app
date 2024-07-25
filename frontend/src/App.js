65af36c73d50c69ba1a11bed6052406d6ee59cbb
Andy <klee682@aucklanduni.ac.nz>
2024-07-25 13:32:27 +1200

    reverting back to previous app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -13,26 +11,28 @@ import Dashboard from './pages/Dashboard';
 import GroupChat from './components/GroupChat';
 import GroupChatInterface from './pages/GroupChat.js';
 import Chat from './pages/Chat';
+import PrivateRoute from './components/PrivateRoute'; 
+import { AuthProvider } from './components/AuthContext'; 
 import './App.css';
 
 function App() {
   return (
-    <>
+    <AuthProvider> 
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/chat/:currentUsername/:friendUsername" element={<PrivateRoute><Chat /></PrivateRoute>} />
-          <Route path="/groupchat/:groupChatName/:currentUsername" element={<PrivateRoute><GroupChatInterface /></PrivateRoute>} />
-          <Route path="/groupchat" element={<PrivateRoute><GroupChat /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+          <Route path="/chat/:currentUsername/:friendUsername" element={<PrivateRoute element={<Chat />} />} />
+          <Route path="/groupchat/:groupChatName/:currentUsername" element={<PrivateRoute element={<GroupChatInterface />} />} />
+          <Route path="/groupchat" element={<PrivateRoute element={<GroupChat />} />} />
         </Routes>
       </Router>
       <Footer />
-    </>
+    </AuthProvider>
   );
 }
 
